acae67ed6ecb72b414c6132225ac434b690512ae
pavanhub-47 <140827035+pavanhub-47@users.noreply.github.com>
2024-07-29 23:26:01 +0530

    second commit

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ team }) => {
     return (
-        <div className="bg-white p-6 rounded-lg shadow-md">
+        <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
             <h3 className="text-xl font-bold mb-2">{team.teamName}</h3>
             <div className="mb-4">
                 <div className="mb-4">
@@ -10,15 +10,17 @@ const Card = ({ team }) => {
                     <p className="text-gray-700">{team.teamLeader}</p>
                 </div>
                 <h4 className="text-lg font-semibold">Team Members:</h4>
-                <ul className="list-disc pl-5">
+                <ul className="list-disc pl-5 space-y-2">
                     {team.teamMembers.map((member, index) => (
-                        <li key={index} className="text-gray-700">
+                        <li
+                            key={index}
+                            className="text-gray-700 hover:text-blue-600 transition-colors duration-300"
+                        >
                             {member}
                         </li>
                     ))}
                 </ul>
             </div>
-
             <div>
                 <h4 className="text-lg font-semibold">Project Topic:</h4>
                 <p className="text-gray-700">{team.projectTopic}</p>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000"
-    }
-  }
-})
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/api": "https://team-backend-q0la.onrender.com",
+        },
+    },
+});
